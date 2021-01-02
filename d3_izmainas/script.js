30ee3ca827dbbc2f26eca46a8804564b59b217a4
MartaRudzate <75455039+MartaRudzate@users.noreply.github.com>
2021-01-02 22:11:18 +0200

    Update script.js

--- d3_izmainas/script.js
+++ d3_izmainas/script.js
@@ -1,105 +1,4 @@
-  myfile = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTVlJJp4jCFbUpEqnafZ-YlI0fgGaA7l7Vde93rPu1GCy4x1NuJiNGjEPMXBxhSOIzmp3CXn_piX-LV/pub?gid=0&single=true&output=csv'
-
-
-
-d3.csv(myfile, function (data) {
-  // Variables
-  var body = d3.select('body')
-	var margin = { top: 50, right: 50, bottom: 50, left: 50 }
-	var h = 500 - margin.top - margin.bottom
-	var w = 500 - margin.left - margin.right
-	var formatPercent = d3.format('.2%')
-	// Scales
-  var colorScale = d3.scale.category20()
-  var xScale = d3.scale.linear()
-    .domain([
-    	d3.min([60,d3.min(data,function (d) { return d.x })]),
-    	d3.max([90,d3.max(data,function (d) { return d.y })])
-    	])
-    .range([0,w])
-  var yScale = d3.scale.linear()
-    .domain([
-    	d3.min([30,d3.min(data,function (d) { return d.x })]),
-    	d3.max([110,d3.max(data,function (d) { return d.y })])
-    	])
-    .range([h,0])
-	// SVG
-	var svg = body.append('svg')
-	    .attr('height',h + margin.top + margin.bottom)
-	    .attr('width',w + margin.left + margin.right)
-	  .append('g')
-	    .attr('transform','translate(' + margin.left + ',' + margin.top + ')')
-	// X-axis
-	var xAxis = d3.svg.axis()
-	  .scale(xScale)
-	  //.tickFormat(formatPercent)
-	  .ticks(5)
-	  .orient('bottom')
-  // Y-axis
-	var yAxis = d3.svg.axis()
-	  .scale(yScale)
-	  //.tickFormat(formatPercent)
-	  .ticks(5)
-	  .orient('left')
-  // Circles
-  var circles = svg.selectAll('circle')
-      .data(data)
-      .enter()
-    .append('circle')
-      .attr('cx',function (d) { return xScale(d.x) })
-      .attr('cy',function (d) {return yScale(d.y) })
-      .attr('r','5')
-      .attr('stroke','black')
-      .attr('stroke-width',1)
-      .attr('fill',function (d,i) { if (d.dzimums == 'Meitene') {return "#FF0000"} else {return "#00FFFF" }})
-      .on('mouseover', function () {
-        d3.select(this)
-          .transition()
-          .duration(500)
-          .attr('r',10)
-          .attr('stroke-width',3)
-      })
-      .on('mouseout', function () {
-        d3.select(this)
-          .transition()
-          .duration(500)
-          .attr('r',5)
-          .attr('stroke-width',1)
-      })
-    .append('title') // Tooltip
-      .text(function (d) { return d.dzimums +
-                           '\nMiera pulss: ' + (d.x) +
-                           '\nFiziskās sagatavotības indekss: ' + (d.y) })
-  // X-axis
-  svg.append('g')
-      .attr('class','axis')
-      .attr('transform', 'translate(0,' + h + ')')
-      .call(xAxis)
-    .append('text') // X-axis Label
-      .attr('class','label')
-      .attr('y',-10)
-      .attr('x',w)
-      .attr('dy','.71em')
-      .style('text-anchor','end')
-      .text('Miera pulss')
-  // Y-axis
-  svg.append('g')
-      .attr('class', 'axis')
-      .call(yAxis)
-    .append('text') // y-axis Label
-      .attr('class','label')
-      .attr('transform','rotate(-90)')
-      .attr('x',0)
-      .attr('y',5)
-      .attr('dy','.71em')
-      .style('text-anchor','end')
-      .text('Fiziskās sagatavotības indekss')
-	
-	
-	
-	
-	
-	var margin = {top: 5, right: 5, bottom: 20, left: 20},
+	  var margin = {top: 5, right: 5, bottom: 20, left: 20},
 	    width = 450 - margin.left - margin.right,
 	    height = 450 - margin.top - margin.bottom;
 
@@ -228,6 +127,9 @@ d3.csv(myfile, function (data) {
       // y-intercept = b = (e - f) / n
       var b = (e - f) / n;
 
+			// Print the equation below the chart
+			document.getElementsByClassName("equation")[0].innerHTML = "y = " + m + "x + " + b;
+			document.getElementsByClassName("equation")[1].innerHTML = "x = ( y - " + b + " ) / " + m;
 
       // return an object of two points
       // each point is an object with an x and y coordinate
@@ -239,5 +141,7 @@ d3.csv(myfile, function (data) {
         ptB : {
           y: minY,
           x: (minY - b) / m
-	
-}
+        }
+      }
+
+    }
