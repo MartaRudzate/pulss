32caff27cac448af70ef8c1a8d423170218490d8
MartaRudzate <75455039+MartaRudzate@users.noreply.github.com>
2021-01-02 22:38:37 +0200

    Create script.js

---
+++ d31/script.js
@@ -0,0 +1,99 @@
+myfile = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQYoW-Iy38kzrY_L6tN2CNqeprUeuTV3he09cMwYHvepiMw3B4_vWCjzje_gN5qxEcUPb1_OyK968yX/pub?gid=0&single=true&output=csv'
+//myfile = 'pulsa_dati.csv'
+
+
+
+d3.csv(myfile, function (data) {
+  // Variables
+  var body = d3.select('body')
+	var margin = { top: 50, right: 50, bottom: 50, left: 50 }
+	var h = 500 - margin.top - margin.bottom
+	var w = 500 - margin.left - margin.right
+	var formatPercent = d3.format('.2%')
+	// Scales
+  var colorScale = d3.scale.category20()
+  var xScale = d3.scale.linear()
+    .domain([
+    	d3.min([60,d3.min(data,function (d) { return d.miera_pulss })]),
+    	d3.max([85,d3.max(data,function (d) { return d.miera_pulss })])
+    	])
+    .range([0,w])
+  var yScale = d3.scale.linear()
+    .domain([
+    	d3.min([70,d3.min(data,function (d) { return d.pulss_videospeles })]),
+    	d3.max([120,d3.max(data,function (d) { return d.pulss_videospeles })])
+    	])
+    .range([h,0])
+	// SVG
+	var svg = body.append('svg')
+	    .attr('height',h + margin.top + margin.bottom)
+	    .attr('width',w + margin.left + margin.right)
+	  .append('g')
+	    .attr('transform','translate(' + margin.left + ',' + margin.top + ')')
+	// X-axis
+	var xAxis = d3.svg.axis()
+	  .scale(xScale)
+	  //.tickFormat(formatPercent)
+	  .ticks(5)
+	  .orient('bottom')
+  // Y-axis
+	var yAxis = d3.svg.axis()
+	  .scale(yScale)
+	  //.tickFormat(formatPercent)
+	  .ticks(5)
+	  .orient('left')
+  // Circles
+  var circles = svg.selectAll('circle')
+      .data(data)
+      .enter()
+    .append('circle')
+      .attr('cx',function (d) { return xScale(d.miera_pulss) })
+      .attr('cy',function (d) { return yScale(d.pulss_videospeles) })
+      .attr('r','5')
+      .attr('stroke','black')
+      .attr('stroke-width',1)
+      .attr('fill',function (d,i) { if (d.Dzimums == 'Meitene') {return "#FF0000"} else {return "#00FFFF" }})
+      .on('mouseover', function () {
+        d3.select(this)
+          .transition()
+          .duration(500)
+          .attr('r',10)
+          .attr('stroke-width',3)
+      })
+      .on('mouseout', function () {
+        d3.select(this)
+          .transition()
+          .duration(500)
+          .attr('r',5)
+          .attr('stroke-width',1)
+      })
+    .append('title') // Tooltip
+      .text(function (d) { return d.Dzimums +
+                           '\nMiera pulss: ' + (d.miera_pulss) +
+                           '\nVidējais pulss videospēļu laikā: ' + (d.pulss_videospeles) })
+  // X-axis
+  svg.append('g')
+      .attr('class','axis')
+      .attr('transform', 'translate(0,' + h + ')')
+      .call(xAxis)
+    .append('text') // X-axis Label
+      .attr('class','label')
+      .attr('y',-10)
+      .attr('x',w)
+      .attr('dy','.71em')
+      .style('text-anchor','end')
+      .text('Miera pulss')
+  // Y-axis
+  svg.append('g')
+      .attr('class', 'axis')
+      .call(yAxis)
+    .append('text') // y-axis Label
+      .attr('class','label')
+      .attr('transform','rotate(-90)')
+      .attr('x',0)
+      .attr('y',5)
+      .attr('dy','.71em')
+      .style('text-anchor','end')
+      .text('Vidējais pulss videospēles laikā')
+      
+ })     
